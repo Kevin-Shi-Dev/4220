28dcab5907297157eec03a8fdff5eab394a79c3d
mjpin7 <mjpin7@gmail.com>
2019-10-11 13:33:35 -0400

    Added unit test and some comments

--- imageSearch/tests.py
+++ imageSearch/tests.py
@@ -11,3 +11,8 @@ class ViewTestCase(TestCase):
     def test_upload_page(self):
         response = self.client.get('/upload')
         self.assertEqual(response.status_code, 200)
+
+    # Test for checking to see if 200 is returned from image page
+    def test_image_page(self):
+        response = self.client.get('/image')
+        self.assertEqual(response.status_code, 200)
