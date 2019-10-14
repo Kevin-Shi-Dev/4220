9363c47070e0dea1b714403598543da585bbd551
Zach Easterbrook <FishLikeHexagon@gmail.com>
2019-10-14 18:13:25 +0000

    Added initial models and migrations for the db structure

--- imageSearch/models.py
+++ imageSearch/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
 # Create your models here.
+class Tags(models.Model):
+    tag_id = models.AutoField(primary_key=True)
+    tag_name = models.CharField(max_length=100)
+    weight = models.DecimalField(max_digits=10, decimal_places=2)
+
+class Images(models.Model):
+    image_id = models.AutoField(primary_key=True)
+    image_hash = models.CharField(max_length=32)
+    upload_time = models.DateTimeField(auto_now_add=True)
+    weight = models.DecimalField(max_digits=10, decimal_places=2)
+    width = models.IntegerField()
+    height = models.IntegerField()
+    image_type = models.CharField(max_length=5)
+    tags = models.ManyToManyField(Tags, through='ImageHasTags')
+
+class Urls(models.Model):
+    url_id = models.AutoField(primary_key=True)
+    short_url = models.CharField(max_length=255)
+    url = models.CharField(max_length=500)
+    image = models.OneToOneField(Images, on_delete=models.CASCADE, to_field='image_id')
+
+class ImageHasTags(models.Model):
+    image = models.ForeignKey(Images, on_delete=models.CASCADE, to_field='image_id')
+    tag = models.ForeignKey(Tags, on_delete=models.CASCADE, to_field='tag_id')
--- mysite/settings.py
+++ mysite/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'HOST': '',
+        'NAME': 'image_search',
+        'PASSWORD': 'password',
+        'PORT': '',
+        'USER': 'root'
     }
 }
 
@@ -106,7 +110,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'EST'
 
 USE_I18N = True
 
